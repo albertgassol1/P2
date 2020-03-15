b90b13353f63329461ca77c98f4871550a42ee26
Albert Gassol Puigjaner <albertgassol1@gmail.com>
2020-03-15 12:52:56 +0100

    Codi final

--- scripts/run_vad.sh
+++ scripts/run_vad.sh
@@ -6,35 +6,25 @@
 
 DB=/home/albert/Documents/uni/3/3B/PAV/lab/P2/db.v4
 CMD=bin/vad  #write here the name and path of your program
-for n in 1 2 3 4 5 6 7 8 9; do
-    for j in 1 2 3 4 5 6 7 8 9; do
-        for k in 1500 1550 1600 1650 1700 1750 1800 1850 1900 1950 2000; do
-            for l in 1 2 3 4 5 6 7 8 9 10; do
-                for m in 1 2 3 4 5 6 7 8 9 10; do
-                    for filewav in $DB/*/*wav; do
-                    #    echo
-                        #echo "**************** $filewav ****************"
-                        if [[ ! -f $filewav ]]; then 
-                            echo "Wav file not found: $filewav" >&2
-                            exit 1
-                        fi
 
-                        filevad=${filewav/.wav/.vad}
-                        $CMD -i $filewav -o $filevad -a $n -b $j -c $k -d $l -e $m 
-                    # Alternatively, uncomment to create output wave files
-                    #    filewavOut=${filewav/.wav/.vad.wav}
-                    #    $CMD $filewav $filevad $filewavOut || exit 1
-                    done
-                    echo "alpha1 = " $n " alpha2 = " $j " zeros = " $k " fSilence = " $l " fVoice = " $m
-                    echo "alpha1 = " $n " alpha2 = " $j " zeros = " $k " fSilence = " $l " fVoice = " $m >> resultados.txt 
-                    scripts/vad_evaluation.pl $DB/*/*lab 
-                    
-                done
-            done
-        done
-    done
+for filewav in $DB/*/*wav; do
+#    echo
+    echo "**************** $filewav ****************"
+    if [[ ! -f $filewav ]]; then 
+	    echo "Wav file not found: $filewav" >&2
+	    exit 1
+    fi
+
+    filevad=${filewav/.wav/.vad}
+    filewavOut=${filewav/.wav/_silence.wav}
+    $CMD -i $filewav -o $filevad  -w $filewavOut -a 4 -b 1 -c 1900 -d 6 -e 8 || exit 1
+
+# Alternatively, uncomment to create output wave files
+#    filewavOut=${filewav/.wav/.vad.wav}
+     #$CMD $filewav $filevad $filewavOut || exit 1
+
 done
 
-
+scripts/vad_evaluation.pl $DB/*/*lab
 
 exit 0
